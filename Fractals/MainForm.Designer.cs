a73e8dd24965be00a55d4dfd5a21827d016cb622
Vladislav Kresov <vladislavkresov@gmail.com>
2019-11-30 20:43:59 +0300

    Добавьте файлы проекта.

---
+++ Fractals/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Fractals
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.header = new System.Windows.Forms.Panel();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.header.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.White;
+            this.header.Controls.Add(this.startBTN);
+            this.header.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.header.Location = new System.Drawing.Point(0, 734);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(1123, 52);
+            this.header.TabIndex = 0;
+            // 
+            // startBTN
+            // 
+            this.startBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startBTN.Location = new System.Drawing.Point(992, 3);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(119, 43);
+            this.startBTN.TabIndex = 0;
+            this.startBTN.Text = "Start";
+            this.startBTN.UseVisualStyleBackColor = true;
+            this.startBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1123, 734);
+            this.canvas.TabIndex = 1;
+            this.canvas.Click += new System.EventHandler(this.canvas_Click);
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 786);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.header);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fractals";
+            this.header.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel header;
+        private System.Windows.Forms.Button startBTN;
+        private System.Windows.Forms.Panel canvas;
+    }
+}
+
---
+++ Fractals/MainForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractals
+{
+    public partial class MainForm : Form
+    { 
+        Pen pen = new Pen(Color.Black,1);
+        Pen topPen = new Pen(Color.Red,3);
+        Graphics g = null;
+        Point startPoint;
+        List<Point> tops = new List<Point>(), points = new List<Point>();
+        bool start = false;
+
+        public MainForm()
+        {
+            this.KeyPreview = true;
+
+            InitializeComponent();
+
+            startPoint = new Point(canvas.Width/2,canvas.Height/2);
+
+            this.KeyDown += new KeyEventHandler(MainForm_KeyDown);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tops.Count > 2)
+            {
+                start = !start;
+                if (start)
+                {
+                    startBTN.Text = "Stop";
+                    Picking();
+                    canvas.Refresh();
+                }
+                else
+                {
+                    startBTN.Text = "Start";
+                }
+                
+            }
+            else
+            {
+                //сообщение
+            }
+        }
+
+        private void Picking()
+        {
+            Random rnd = new Random();
+            Point point = startPoint;
+            addPoint(point);
+            for (int i = 0; i < 100000; i++)
+            {
+                int n = rnd.Next(0, tops.Count);
+                int x = Math.Min(point.X, tops[n].X) + Math.Abs(point.X - tops[n].X) / 2;
+                int y = Math.Min(point.Y, tops[n].Y) + Math.Abs(point.Y - tops[n].Y) / 2;
+                point = new Point(x, y);
+                addPoint(point);
+            }
+            startPoint = point;
+        }
+
+        private void canvas_Click(object sender, EventArgs e)
+        {
+            if (!start)
+            {
+                Point point = canvas.PointToClient(Cursor.Position);
+                addTop(point);
+                canvas.Refresh();
+            }
+        }
+
+        private void addTop(Point point)
+        {
+            this.tops.Add(point);
+        }
+
+        private void addPoint(Point point)
+        {
+            this.points.Add(point);
+        }
+
+        private void deleteLastTop()
+        {
+            if(tops.Count>0)
+            this.tops.RemoveAt(tops.Count - 1);
+        }
+
+        private void deletePoints()
+        {
+            this.points.Clear();
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.Z)
+            {
+                if (!start)
+                {
+                    deleteLastTop();
+                    deletePoints();
+                    startPoint = new Point(new Random().Next(1, canvas.Width), new Random().Next(1, canvas.Height));
+                    canvas.Refresh();
+                }
+            }
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            g = canvas.CreateGraphics();
+
+            for (int i = 0; i < tops.Count; i++)
+            {
+                Point point = tops[i];
+                g.DrawEllipse(topPen, point.X, point.Y, 1, 1);
+
+                if (i == 0)
+                {
+                    g.DrawLine(pen, point, tops[tops.Count-1]);
+                }
+                else
+                {
+                    g.DrawLine(pen, point, tops[i - 1]);
+                }
+            }
+
+            foreach(Point point in points)
+            {
+                g.DrawRectangle(pen, point.X, point.Y, 1, 1);
+            }
+
+        }
+
+    }
+}
